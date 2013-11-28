8e283148c77b1ec4f5e63c332b6f16d1c7a00d0c
Dibyo Mukherkee <dibyo.m@gmail.com>
2013-11-28 10:06:21 -0500

    EnergyPlus runner v1.0

--- src/building/RunEnergyPlus.java
+++ src/building/RunEnergyPlus.java
@@ -1,4 +1,32 @@
+package building;
+import org.apache.commons.exec.*;
+import java.lang.Runtime;
+import java.io.*;
+
 public class RunEnergyPlus
 {
-	
+	public void execute() throws java.io.IOException
+	{
+		CommandLine cmdLine = new CommandLine("runenergyplus");
+		cmdLine.addArgument("bentley.idf");
+		cmdLine.addArgument("USA_IL_Chicago-OHare.Intl.AP.725300_TMY3.epw");
+		
+		// cmdLine.addArgument("${file}");
+		// HashMap map = new HashMap();
+		// map.put("file", new File("invoice.pdf"));
+		// commandLine.setSubstitutionMap(map);
+
+		DefaultExecuteResultHandler resultHandler = new DefaultExecuteResultHandler();
+
+		ExecuteWatchdog watchdog = new ExecuteWatchdog(600*1000);
+		Executor executor = new DefaultExecutor();
+		executor.setExitValue(1);
+		executor.setWatchdog(watchdog);
+		executor.execute(cmdLine, resultHandler);
+
+		// some time later the result handler callback was invoked so we
+		// can safely request the exit value
+		int exitValue = resultHandler.getExitValue();
+		System.out.println(exitValue);	
+	}
 }
