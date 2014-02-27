a343bffcf45c56603456191f14899f02b5dc6965
Dibyo Mukherkee <dibyo.m@gmail.com>
2014-02-27 01:32:54 -0500

    Dist done

---
+++ Assumptions.md
@@ -0,0 +1,12 @@
+Assumptions
+===========
+
+* Construction for wall would have to follow the following pattern
+	Construction,
+		Name-has-to-contain-wall,		!-name
+		Layer1-Material,				!-Layer 1
+		Layer2-Material,				!-Layer 2
+		Layer3-Material;				!-Layer 3
+
+We are only changing the last layer with something more robust.
+
--- src/building/Building.java
+++ src/building/Building.java
@@ -13,42 +13,53 @@ public class Building extends Problem implements SimpleProblemForm {
 
 	public void evaluate(final EvolutionState state, final Individual ind, final int subpopulation, final int threadnum)
 	{
-		if( !( ind instanceof DoubleVectorIndividual ) )
-            state.output.fatal( "The individuals for this problem should be DoubleVectorIndividuals." );
+		
+    //Ensure that the individual is of right type
+    if( !( ind instanceof DoubleVectorIndividual ) )
+      state.output.fatal( "The individuals for this problem should be DoubleVectorIndividuals." );
 
-        DoubleVectorIndividual temp = (DoubleVectorIndividual)ind;
-        double[] genome = temp.genome;
-        int numDecisionVars = genome.length;
-        if(numDecisionVars!=1) throw new RuntimeException("Building needs exactly 1 decision variables (genes).");
+    float[] objectives = ((MultiObjectiveFitness)ind.fitness).getObjectives();
+    // Copy of ind
+    DoubleVectorIndividual temp = (DoubleVectorIndividual)ind;
+    double[] genome = temp.genome;
 
-        float[] objectives = ((MultiObjectiveFitness)ind.fitness).getObjectives();
-  
-        try
-        {
-          File idf = IDFHelper.modifyIDF(genome);
-          RunEnergyPlus.execute(idf);
-        }
-        catch(IOException e)
-        {
-          e.printStackTrace();
-        }
+    // Ensure the number of decision variables is 4
+    int numDecisionVars = genome.length; 
+    if(numDecisionVars!=1) throw new RuntimeException("Building needs exactly 1 decision variables (genes).");
 
-        
-        String[] lastRow = IDFHelper.parseBuildingCSV();
-        double electricity = Double.parseDouble(lastRow[1]);
-        double natural_gas = Double.parseDouble(lastRow[16]);
-        double energy = electricity + natural_gas;
-        double cost = 1 * electricity + 0.7 * natural_gas;
+    
+    // * Create a modified IDF with the genomes and run E+ on the modified file.
+    // * @TODO change the method name to something more relevant
+    // * @TODO Have the execute return the output file.
+     
+    try
+    {
+      //File idf = IDFHelper.modifyIDF(genome);
+      //RunEnergyPlus.execute(idf);
+      //File file = RunEnergyPlus.execute(idf);
+    }
+    catch(IOException e)
+    {
+      e.printStackTrace();
+    }
 
-        objectives[0] = (float) energy; 
-       	objectives[1] = (float) cost;
+    // Get cost and energy usage values from the building. 
+    String[] lastRow = IDFHelper.parseBuildingCSV();
+    double electricity = Double.parseDouble(lastRow[1]);
+    double natural_gas = Double.parseDouble(lastRow[16]);
+    double energy = electricity + natural_gas;
+    double cost = 1 * electricity + 0.7 * natural_gas;
 
-       	((MultiObjectiveFitness)ind.fitness).setObjectives(state, objectives);
-        ind.evaluated = true;
-	}
-	
+    //Set the objectives 
+    objectives[0] = (float) energy; 
+    objectives[1] = (float) cost;
+
+    ((MultiObjectiveFitness)ind.fitness).setObjectives(state, objectives);
+    ind.evaluated = true;
+  }
+
   public static void compute_costs()
   {
-    
+
   }
 }
